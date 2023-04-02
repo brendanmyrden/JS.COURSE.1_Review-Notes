b648da6fc56b9ab29fcc12462bec7a367c6f5329
brendanmyrden <brendan4myrden@proton.me>
2023-04-02 19:28:55 -0230

    Update to README, Additions to .js : Alerts, Nums, Strings, Booleans, Undefined, Math Operators, Str Concatonation, and Assignment Operators

---
+++ 01/review_starter_01.js
@@ -0,0 +1,80 @@
+// ALERTS
+
+let alerting = 'I Am Alert';
+
+// if(alerting === "I Am Alert") alert (`Stay alert, your enemy prowls like a roaring lion looking for someone to devour.`)
+
+
+// NUMBERS, STRINGS
+
+console.log(2 + 10 + 4 + 1);
+
+var firstName = `Joe`;
+// console.log(firstName);
+
+var firstName = `Fib`;
+console.log(firstName); 
+
+// Can reassign without using let or const
+
+const myFirstJob = `Mowing Lawns`;
+const myCurrentJob = `Front End Programmer`;
+
+
+// BOOLEAN, REASSIGNING
+
+let jsIsFun = true;
+jsIsFun = `Yep, it's fun if you stay with it!`; // To reassign you don't need to use a let or var, just the variable name. No using const.
+
+console.log(jsIsFun);
+
+
+// UNDEFINED
+
+let yes;
+console.log(yes);
+
+yes = `Sure`;
+console.log(yes);
+console.log(typeof yes);
+
+
+// MATH OPERATORS, STRING CONCATONATION
+
+const thisYear = 2023;
+const brendansBirthYear = 1994;
+const brendansAge = thisYear - brendansBirthYear;
+
+console.log(`Brendan's age turns out to be ${brendansAge}`); // Template string example
+
+console.log(brendansAge * 2, 2 ** 3);
+
+
+lastName = `Myrden`;
+console.log(firstName + ' ' + lastName);
+// better way is a template string
+
+
+// ASSIGNMENT OPERATORS 
+
+let x = 10 + 6;
+console.log(x);
+
+x += 10;
+console.log(x);
+
+x -= 1;
+console.log(x);
+
+x *= 4;
+console.log(x);
+
+x /= 4;
+console.log(x);
+
+x ++;
+console.log(x);
+
+x --;
+console.log(x);
+
