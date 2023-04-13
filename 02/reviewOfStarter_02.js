6c86dc4b372f625a361d54cec4284e8e4dfa95d0
brendanmyrden <brendan4myrden@proton.me>
2023-04-12 22:41:16 -0230

    Update to .html, Function Declarations review.

--- 02/reviewOfStarter_02.js
+++ 02/reviewOfStarter_02.js
@@ -10,8 +10,31 @@ logger(); // This is named : calling, invoking, or running the function
 // FUNCTIONS 
 
 function veggieStirfry (broccoli, carrot) {
+
     const mix = console.log(`Stirfry with ${broccoli} ${broccoli === 1 ? `piece` : `pieces`} of broccoli and ${carrot} ${carrot === 1 ? `piece` : `pieces`} of carrot.`);
+    
     return mix; 
 }
 
-veggieStirfry(1,4);
+const vegMix = veggieStirfry(1,4);
+
+const broccoliMix = veggieStirfry(7, 0);
+
+
+function animalLover (dogs, cats, horses, rabbits) {
+
+    const pets = console.log(`We have ${dogs} ${dogs === 1 ? `dog` : `dogs`}, ${cats} ${cats === 1 ? `cat` : `cats`}, ${horses} ${horses === 1 ? `horse` : `horses`}, and ${rabbits} ${rabbits === 1 ? `rabbit` : `rabbits`} on our ranch!`);
+
+}
+
+const manyAnimals = animalLover(5, 6, 1, 3);
+const aCoupleAnimals = animalLover(2, 2, 0, 1); // Because the template string is built into the function and is wrapped by console.log() it is printed automatically in the dev console.
+
+
+// FUNCTION DECLARATIONS
+
+function whatIsYourName (nameOfPerson) {
+    const name = console.log(`Your name is ${nameOfPerson}, what a great name!`);
+}
+
+whatIsYourName(`Bernard`);
