42ce2da71b0eb9d83501cfaf61836c33a4b8fba2
brendanmyrden <brendan4myrden@proton.me>
2023-06-29 09:05:05 -0230

    Assignment Review Correction

--- 02/Review_Assignments/reviewOfAssignments_02.js
+++ 02/Review_Assignments/reviewOfAssignments_02.js
@@ -1,6 +1,9 @@
 'use strict';
 
-// PART 2 - CODING CHALLENGE 1
+// PART 2 - FUNDAMENTALS
+
+
+// CODING CHALLENGE 1
 
 const calcAverage = (a,b,c) => (a + b + c ) / 3;
 
@@ -39,3 +42,51 @@ avgKoalas = calcAverage(23, 34, 27);
 
 checkWinner(avgDolphins, avgKoalas);
 
+
+// CODING CHALLENGE 2
+
+// TIP CALCULATOR w. ARRAY
+
+const calcTip = (bill) => bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.20;
+console.log(calcTip (100));
+
+const bills = [125, 555, 44];
+console.log(bills);
+
+const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+console.log(tips);
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals);
+
+
+// CODING CHALLENGE 3 
+
+// OBJECTS + BMI
+
+const mark = {
+
+    fullName: `Mark Miller`,
+    mass: 78,
+    height: 1.69,
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height ** 2);
+        return this.bmi;
+    }
+}
+
+
+const john = {
+
+    fullName: `John Smith`,
+    mass: 92,
+    height: 1.95,
+    
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height ** 2);
+        return this.bmi;
+    }
+}
+
+const getSumOfBMIs = `${john.calcBMI()} > ${mark.calcBMI()} ? `${john.fullName}'s BMI at ${john.calcBMI()}` : `${mark.fullName}'s BMI at ${mark.BMI()}`is the highest`
